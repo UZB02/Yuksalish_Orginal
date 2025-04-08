5d7fff6ad8e877ad6fb07b1af651d64d1ffd4784
UZB02 <muhsinbekmirzamatov3@gmail.com>
2025-04-08 10:19:05 +0500

    Barn done

--- src/components/BarnComponents/BarnProductIdTable.vue
+++ src/components/BarnComponents/BarnProductIdTable.vue
@@ -1,62 +1,132 @@
 <template>
-  <div class="relative overflow-x-auto shadow-md rounded">
-    <table class="min-w-[1400px] text-sm text-left text-gray-500 dark:text-gray-400">
-      <thead class="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-        <tr>
-          <th scope="col" class="px-2 py-3">Haridor</th>
-          <th scope="col" class="px-2 w-40 py-3">Tell</th>
-          <th scope="col" class="px-2 w-28 py-3">Sotildi KG</th>
-          <th scope="col" class="px-2 w-32 py-3 ">1 kg uchun</th> <th scope="col" class="px-2 w-40 py-3">Asl narxi 1 kg</th>
-          <th scope="col" class="px-2 min-w-52 py-3">Tushkan summa</th>
-          <th scope="col" class="px-2 min-w-52 py-3">Sof foyda</th>
-          <th scope="col" class="px-2 w-40 py-3">Sotilgan vaqti</th>
-          <th scope="col" class="px-2 py-3">O'chirish</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr v-for="(item, itemKey) in productHistory?.history" :key="itemKey" class="odd:bg-white even:bg-gray-50 border-b dark:border-gray-700">
-          <th scope="row" class="px-2 py-4 font-medium text-gray-900 dark:text-white w-56 overflow-hidden whitespace-nowrap text-ellipsis">{{ item?.name }}</th>
-          <td class="px-2 py-4">{{ item?.phone }}</td>
-          <td class="px-2 py-4">{{ item?.size }} kg</td>
-          <td class="px-2 py-4">{{ formatCurrency(item?.sellingPrice) }}</td>
-          <td class="px-2 py-4">{{ formatCurrency(item?.originalPrice) }}</td>
-          <td class="px-2 py-4">{{ formatCurrency(item?.totalAmount) }}</td>
-          <td class="px-2 py-4">{{ formatCurrency(item?.profit) }}</td>
-          <td class="px-2 py-4">{{ formatDateTime(item?.createdAt) }}</td>
-          <td class="px-2 py-4 flex justify-center gap-2">
-            <button @click="deleteProductHistory(item?._id)"><i class="pi pi-trash text-red-500"></i></button>
-            <button @click="viewDescription(item)"><i class="pi pi-eye"></i></button>
-          </td>
-        </tr>
-        <tr v-if="!productHistory?.history?.length">
-          <td colspan="9" class="px-6 py-4 text-center text-gray-500 dark:text-gray-400">Sotilgan ma'lumotlar yo'q</td>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-
-  <!-- Paginator -->
-  <Paginator :rows="limit" :totalRecords="totalItems" :rowsPerPageOptions="[5, 10, 20, 30]" @page="onPageChange" />
-
-  <!-- Modal View -->
-  <div v-if="viewVisible" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-    <div class="bg-white p-4 rounded-lg shadow-lg w-96">
-      <div class="flex justify-between items-center border-b pb-2">
-        <h3 class="text-lg font-semibold">{{ viewProduct?.name }}</h3>
-        <button @click="viewVisible = false">✖</button>
-      </div>
-      <p class="mt-4">{{ viewProduct?.description || "Tafsilotlar mavjud emas!" }}</p>
+    <div class="relative overflow-x-auto shadow-md rounded">
+        <!--Begin Skeleton loading table -->
+        <DataTable v-if="loadingProduct" :value="Array(5).fill({})" scrollable tableStyle="min-width: 1800px">
+            <Column header="Haridor">
+                <template #body>
+                    <Skeleton width="100px" height="1rem" />
+                </template>
+            </Column>
+            <Column header="Tell">
+                <template #body>
+                    <Skeleton width="100px" height="1rem" />
+                </template>
+            </Column>
+            <Column header="Sotilgan Mahsulot">
+                <template #body>
+                    <Skeleton width="80px" height="1rem" />
+                </template>
+            </Column>
+            <Column header="Sotilish Narxi">
+                <template #body>
+                    <Skeleton width="100px" height="1rem" />
+                </template>
+            </Column>
+            <Column header="Tannarxi">
+                <template #body>
+                    <Skeleton width="100px" height="1rem" />
+                </template>
+            </Column>
+            <Column header="Tushkan Summa">
+                <template #body>
+                    <Skeleton width="120px" height="1rem" />
+                </template>
+            </Column>
+            <Column header="Tushkan Foyda">
+                <template #body>
+                    <Skeleton width="120px" height="1rem" />
+                </template>
+            </Column>
+            <Column header="Sotilgan Vaqti">
+                <template #body>
+                    <Skeleton width="150px" height="1rem" />
+                </template>
+            </Column>
+            <Column header="Amallar">
+                <template #body>
+                    <Skeleton shape="circle" size="2rem" class="mr-2" />
+                    <Skeleton shape="circle" size="2rem" />
+                </template>
+            </Column>
+        </DataTable>
+        <!--End Skeleton loading table -->
+        <DataTable v-else :value="productHistory?.history" scrollable tableStyle="min-width: 1800px">
+            <Column field="name" header="Haridor"></Column>
+            <Column field="phone" header="Tell"></Column>
+            <Column field="size" header="Sotilgan Mahsulot">
+                <template #body="slotProps"> {{ slotProps.data.size }} Kg </template>
+            </Column>
+            <Column field="sellingPrice" header="Sotilish Narxi">
+                <template #body="slotProps">
+                    {{ formatCurrency(slotProps.data.sellingPrice) }}
+                </template>
+            </Column>
+            <Column field="originalPrice" header="Tannarxi">
+                <template #body="slotProps">
+                    {{ formatCurrency(slotProps.data.originalPrice) }}
+                </template>
+            </Column>
+            <Column field="totalAmount" header="Tushkan Summa">
+                <template #body="slotProps">
+                    {{ formatCurrency(slotProps.data.totalAmount) }}
+                </template>
+            </Column>
+            <Column field="profit" header="Tushkan Foyda">
+                <template #body="slotProps">
+                    {{ formatCurrency(slotProps.data.profit) }}
+                </template>
+            </Column>
+            <Column field="createdAt" header="Sotilgan Vaqti">
+                <template #body="slotProps">
+                    {{ formatDateTime(slotProps.data.createdAt) }}
+                </template>
+            </Column>
+            <Column header="Amallar">
+                <template #body="slotProps">
+                    <button @click="deletProductHistoryModal(slotProps.data)">
+                        <i class="pi pi-trash text-red-500 mr-2"></i>
+                    </button>
+                    <button @click="viewDescription(slotProps.data)">
+                        <i class="pi pi-eye"></i>
+                    </button>
+                </template>
+            </Column>
+        </DataTable>
     </div>
-  </div>
+    <!--Begin Delete Modal -->
+    <Dialog header="O'chirish" v-model:visible="deletModal" :style="{ width: '350px' }" :modal="true">
+        <div class="flex items-center justify-center">
+            <i class="pi pi-exclamation-triangle mr-4" style="font-size: 2rem" />
+            <span>O'chirishga ishonchingiz komilmi?</span>
+        </div>
+        <template #footer>
+            <Button label="Yo'q" icon="pi pi-times" @click="deletModal = false" text severity="secondary" />
+            <Button :label="isloading ? 'Loading...' : 'O\'chirish'" icon="pi pi-trash" @click="deleteProductHistory()" severity="danger" outlined autofocus />
+        </template>
+    </Dialog>
+    <!--End Delete Modal -->
+    <!-- Paginator -->
+    <Paginator :rows="limit" :totalRecords="totalItems" :rowsPerPageOptions="[5, 10, 20, 30]" @page="onPageChange" />
+
+    <!-- Modal View -->
+    <Dialog header="Tafsilot" v-model:visible="viewVisible" :breakpoints="{ '960px': '75vw' }" :style="{ width: '30vw' }" :modal="true">
+        <h3 class="text-lg font-semibold">{{ viewProduct?.name }}</h3>
+        <p class="mt-4">{{ viewProduct?.description || 'Tafsilotlar mavjud emas!' }}</p>
+        <template #footer>
+            <Button severity="danger" label="Yopish" @click="viewVisible = false" />
+        </template>
+    </Dialog>
+    <!-- Toast -->
+    <Toast />
 </template>
 
 <script setup>
-import axios from "axios";
-import { ref, onMounted } from "vue";
-import { useRoute } from "vue-router";
-import formatCurrency from "../../utils/PriceFormatter";
-import formatDateTime from "../../utils/DateTimeFormatter";
-// import Swal from "sweetalert2";
+import axios from 'axios';
+import { useToast } from 'primevue/usetoast';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import formatDateTime from '../../utils/DateTimeFormatter';
+import formatCurrency from '../../utils/PriceFormatter';
 
 const id = useRoute().params.id;
 const productHistory = ref(null);
@@ -64,37 +134,55 @@ const page = ref(1);
 const limit = ref(5);
 const totalItems = ref(0);
 const viewVisible = ref(false);
+const deletModal = ref(false);
 const viewProduct = ref(null);
+const isloading = ref(false);
+const loadingProduct = ref(true);
+const delProduct = ref({});
+const toast = useToast();
 
 const getProductHistory = async () => {
-  try {
-    const response = await axios.get(`/api/product-history/${id}?page=${page.value}&limit=${limit.value}`);
-    productHistory.value = response.data;
-    totalItems.value = response.data.total;
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const response = await axios.get(`/api/product-history/${id}?page=${page.value}&limit=${limit.value}`);
+        if ((response.status = 200)) {
+            loadingProduct.value = false;
+            productHistory.value = response.data;
+            totalItems.value = response.data.total;
+        }
+    } catch (error) {
+        console.error(error);
+    }
 };
 
-const deleteProductHistory = async (itemId) => {
-  try {
-    await axios.delete(`/api/product-history/delete/${itemId}`);
-    Swal.fire({ position: "top-center", icon: "success", title: "Ma'lumot o'chirildi", showConfirmButton: false, timer: 1500 });
-    getProductHistory();
-  } catch (error) {
-    console.error(error);
-  }
+const deletProductHistoryModal = (item) => {
+    deletModal.value = true;
+    delProduct.value = item;
+    console.log(delProduct.value._id);
+};
+const deleteProductHistory = async () => {
+    isloading.value = true;
+    try {
+        const res = await axios.delete(`/api/product-history/delete/${delProduct.value._id}`);
+        if ((res.status = 200)) {
+            isloading.value = false;
+            toast.add({ severity: 'success', summary: 'Bajarildi', detail: delProduct.value.name + ' ' + "tarixi o'chirildi", life: 3000 });
+            deletModal.value = false;
+            getProductHistory();
+        }
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 const viewDescription = (item) => {
-  viewProduct.value = item;
-  viewVisible.value = true;
+    viewProduct.value = item;
+    viewVisible.value = true;
 };
 
 const onPageChange = (event) => {
-  page.value = event.page + 1;
-  limit.value = event.rows;
-  getProductHistory();
+    page.value = event.page + 1;
+    limit.value = event.rows;
+    getProductHistory();
 };
 
 onMounted(getProductHistory);
--- src/views/Barn/BarnProductIdView.vue
+++ src/views/Barn/BarnProductIdView.vue
@@ -1,76 +1,64 @@
 <template>
-  <div
-    class="w-full grid grid-cols-2 md:flex md:justify-between gap-5 flex-wrap mb-6 md:mt-2 "
-  >
-    <div>
-      <p class="text-xs uppercase text-gray-400">Mahsulot nomi</p>
-      <p class="text-sm font-medium">
-        {{ data.name ? data.name : "Loading..." }}
-      </p>
+    <div class="w-full grid grid-cols-2 md:flex md:justify-between gap-5 flex-wrap mb-6 md:mt-2">
+        <div>
+            <p class="text-xs uppercase text-gray-400">Mahsulot nomi</p>
+            <p class="text-sm font-medium">
+                {{ data.name ? data.name : 'Loading...' }}
+            </p>
+        </div>
+        <div>
+            <p class="text-xs uppercase text-gray-400">Miqdor</p>
+            <p class="text-sm font-medium">{{ data.size ? data.size : 'Loading...' }} kg</p>
+        </div>
+        <div>
+            <p class="text-xs uppercase text-gray-400">1 kg narx</p>
+            <p class="text-sm font-medium">
+                {{ data.price ? formatCurrency(Number(data?.price)) : 'Loading...' }}
+            </p>
+        </div>
+        <div>
+            <p class="text-xs uppercase text-gray-400">1 kg Sotish narxi</p>
+            <p class="text-sm font-medium">
+                {{ data.buyyingPrice ? formatCurrency(Number(data?.buyyingPrice)) : 'Loading...' }}
+            </p>
+        </div>
+        <div>
+            <p class="text-xs uppercase text-gray-400">Jami Summa</p>
+            <p class="text-sm font-medium">
+                {{ data.size ? formatCurrency(Number(data?.size) * Number(data?.price)) : 'Loading...' }}
+            </p>
+        </div>
+        <div>
+            <p class="text-xs uppercase text-gray-400">qo'shilgan vaqt</p>
+            <p class="text-sm font-medium">
+                {{ data.created_at ? formatDateTime(data.created_at) : 'Loading...' }}
+            </p>
+        </div>
     </div>
-    <div>
-      <p class="text-xs uppercase text-gray-400">Miqdor</p>
-      <p class="text-sm font-medium">
-        {{ data.size ? data.size : "Loading..." }} kg
-      </p>
-    </div>
-    <div>
-      <p class="text-xs uppercase text-gray-400">1 kg narx</p>
-      <p class="text-sm font-medium">
-        {{ data.price ? formatCurrency(Number(data?.price)) : "Loading..." }}
-      </p>
-    </div>
-    <div>
-      <p class="text-xs uppercase text-gray-400">1 kg Sotish narxi</p>
-      <p class="text-sm font-medium">
-        {{
-          data.buyyingPrice
-            ? formatCurrency(Number(data?.buyyingPrice))
-            : "Loading..."
-        }}
-      </p>
-    </div>
-    <div>
-      <p class="text-xs uppercase text-gray-400">Jami Summa</p>
-      <p class="text-sm font-medium">
-        {{
-          data.size
-            ? formatCurrency(Number(data?.size) * Number(data?.price))
-            : "Loading..."
-        }}
-      </p>
-    </div>
-    <div>
-      <p class="text-xs uppercase text-gray-400">qo'shilgan vaqt</p>
-      <p class="text-sm font-medium">
-        {{ data.created_at ? formatDateTime(data.created_at) : "Loading..." }}
-      </p>
-    </div>
-  </div>
-  <ProductIdTable class="w-full"/>
+    <ProductIdTable />
 </template>
 <script setup>
-import axios from "axios";
-import { ref, onMounted } from "vue";
-import { useRoute, RouterLink } from "vue-router";
-import formatCurrency from "../../utils/PriceFormatter";
-import formatDateTime from "../../utils/DateTimeFormatter";
-import ProductIdTable from "../../components/BarnComponents/BarnProductIdTable.vue";
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import { useRoute } from 'vue-router';
+import ProductIdTable from '../../components/BarnComponents/BarnProductIdTable.vue';
+import formatDateTime from '../../utils/DateTimeFormatter';
+import formatCurrency from '../../utils/PriceFormatter';
 const id = useRoute().params.id;
 console.log(id);
 const data = ref([]);
 
 const getProduct = async () => {
-  try {
-    const response = await axios.get(`/api/product/${id}`);
-    data.value = response.data;
-    console.log(data.value);
-  } catch (error) {
-    console.error(error);
-  }
+    try {
+        const response = await axios.get(`/api/product/${id}`);
+        data.value = response.data;
+        console.log(data.value);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 onMounted(() => {
-  getProduct();
+    getProduct();
 });
 </script>
